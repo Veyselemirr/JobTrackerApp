6082a09ad803f7b124084d0608a733aea879b415
Veyselemirr <veyselemir_yurtseven@hotmail.com>
2025-05-24 20:26:27 +0300

    create a architecture

---
+++ JobTrackerApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using JobTrackerApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTrackerApp.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet: Veritabanında tablolara karşılık gelen sınıflar
+        public DbSet<User> Users => Set<User>();  // Users tablosu
+        public DbSet<JobApplication> JobApplications => Set<JobApplication>();  // JobApplications tablosu
+
+        // Model yaratılırken özel ayarlar yapılabilir (İlişkiler, kısıtlamalar vs.)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Örnek: Email alanını unique yapabiliriz (ileride Fluent API ile eklenir)
+            modelBuilder.Entity<User>()
+                        .HasIndex(u => u.Email)
+                        .IsUnique();
+        }
+    }
+}
